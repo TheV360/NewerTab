b555a07df291dcd996091e0ce6249e430d60605a
V360 <v360code@gmail.com>
2019-04-08 13:55:16 -0400

    It just works.

--- script.js
+++ script.js
@@ -670,7 +670,7 @@ function doSearch(newtab = false) {
 function transitionTo(url) {
 	if (!settings.transitions) location.assign(url);
 	
-	window.setTimeout(500, ()=>{location.assign(url);});
+	window.setTimeout(location.assign, 500, url);
 	fadeout.classList.add("on");
 }
 
